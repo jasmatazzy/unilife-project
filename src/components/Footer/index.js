473f38ea1a2ed58e48b7cbe1154bfb5855d25548
Jasmine Hendricks <jasmine@justappraised.com>
2022-12-14 15:46:16 -0700

    set up footer component base

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./style.css"
+
+const Footer = () =>
+    <footer>
+        <section id="footerTop">
+        <container id="keep-in-touch">
+            <h1>Keep in touch</h1>
+            <p>Curious about new offerings? Sign up for our weekly newsletter and stay informed.</p>
+            <input type="text" placeholder="Email"></input>
+        </container>
+        <container id="socialize" color="white">
+            <h1>Socialize</h1>
+        </container>
+        </section>
+        <section id="footerBase">
+        Footsie
+        </section>
+    </footer>
+
+
+export default Footer
