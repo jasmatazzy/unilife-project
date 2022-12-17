536f03b10ee9a1c47f300408ddb026d412fc0b14
Jasmine Hendricks <jasmine@justappraised.com>
2022-12-16 18:30:43 -0700

    removed bad h1 styling

--- src/components/CityCard/index.js
+++ src/components/CityCard/index.js
@@ -5,18 +5,17 @@ import './style.css'
 
 
 
-const CityCard = ({city}) => {
+const CityCard = ({ city }) => {
 
-  const cityCardStyle={
+  const cityCardStyle = {
     color: "white",
     textAlign: "center",
     height: "100px",
     fontSize: "12px",
     border: "dotted black 2px",
     backgroundImage: `url("${city.image_url}")`,
-
-    
   }
+  
   return (
     <div id="cityCard" style={cityCardStyle}>
       <h1>{city.name}</h1>
